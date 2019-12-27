1d34699e523422839573d7a6df8e877b18619a52
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2019-12-27 14:04:11 +0530

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -3,20 +3,22 @@ import pickle
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 options = Options()
-options.add_argument("user-data-dir=/Users/vaibhavchopra/Library/Application Support/Google/Chrome/Default")
-
-
+options.add_argument("user-data-dir=<insert profile path here>")
 
 class insta_bot:
-    def login(self):
-        self.driver.get("https://www.instagram.com/accounts/login/")
+    def follow_multiple_from_file(self,filename):
+        profiles=self.read_profiles_from_file(filename)
+        for profile in profiles:
+            self.follow(profile)
+    def login(self,password):
+        password=input("enter password:")
+        self.driver.get(self.base_link+"accounts/login/")
         time.sleep(3.5)
         username_field=self.driver.find_element_by_name("username")
         username_field.send_keys(self.username)
         password_field=self.driver.find_element_by_name("password")
-        password_field.send_keys(self.password)
+        password_field.send_keys(password)
         time.sleep(3.5)
         login_button=self.driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]""")
         login_button.click()
@@ -28,7 +30,7 @@ class insta_bot:
         return int("".join(temp))
 
     def like_recent_of_given(self,profile):
-        self.driver.get("https://www.instagram.com/"+profile)
+        self.driver.get(self.base_link+profile)
         time.sleep(3)
         posts_count_string=self.driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/header/section/ul/li[1]""").text
         posts_count=self.count_calc(posts_count_string)
@@ -43,12 +45,12 @@ class insta_bot:
             print("no posts were there to like for {}".format(profile))
 
     def like_recent_of_following(self):
-        following=self.read_following_from_file()
+        following=self.read_profiles_from_file("following")
         for profile in following:
             self.like_recent_of_given(profile)
 
     def get_following(self):
-        self.driver.get("https://www.instagram.com/"+self.username)
+        self.driver.get(self.base_link+self.username)
         time.sleep(3)
         following_elem=self.driver.find_element_by_partial_link_text("following")
         self.following_count=self.count_calc(following_elem.text.split()[0])
@@ -57,7 +59,7 @@ class insta_bot:
         return self.get_profiles()
 
     def get_followers(self):
-        self.driver.get("https://www.instagram.com/"+self.username)
+        self.driver.get(self.base_link+self.username)
         time.sleep(3)
         followers_elem=self.driver.find_element_by_partial_link_text("followers")
         self.followers_count=self.count_calc(followers_elem.text.split()[0])-1
@@ -79,22 +81,27 @@ class insta_bot:
             idx=f.index("\n")
             profiles.append(f[:idx])
         return profiles
-    def save_following_to_file(self):
-        f=open("following","wb")
+    def save_profiles_to_file(self,filename):
+        f=open(filename,"wb")
         profiles=self.get_following()
         pickle.dump(profiles,f)
         f.close()
-    def save_followers_to_file(self):
-        f=open("followers","wb")
-        profiles=self.get_followers()
-        pickle.dump(profiles,f)
-        f.close()
-    def read_following_from_file(self):
-        f=open("following","rb")
-        return(pickle.load(f))
-    def __init__(self,username,password):
+    def read_profiles_from_file(self,filename):
+        
+        if filename.endswith(".txt"):
+            f=open(filename,'r')
+            profiles=f.readlines()
+            for profile in profiles:
+                profile.strip("\n")
+                profile.strip(" ")
+            while "" in profiles:
+                profiles.remove("")
+            return profiles
+        else:
+            f=open(filename,"rb")
+            return(pickle.load(f))
+    def __init__(self,username):
         self.username=username
-        self.password=password
         self.driver = webdriver.Chrome(options=options)
         self.base_link="https://www.instagram.com/"
     def follow(self,profile):
@@ -106,5 +113,4 @@ class insta_bot:
                 button.click()
                 time.sleep(2)
                 break
-
-bot=insta_bot("_choprautomated_","Password@123")
+bot=insta_bot("username")
